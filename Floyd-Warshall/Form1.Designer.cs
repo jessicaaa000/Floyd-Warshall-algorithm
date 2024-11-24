41d6f92fc497f420039cda7d4df6a06e1806bc4b
Jessica Pawowska <jessica.pawlowska0@gmail.com>
2024-11-24 22:54:26 +0100

    First draft of GUI

---
+++ Floyd-Warshall/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace Floyd_Warshall
+{
+    partial class Background
+    {
+        /// <summary>
+        /// Wymagana zmienna projektanta.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Wyczyść wszystkie używane zasoby.
+        /// </summary>
+        /// <param name="disposing">prawda, jeżeli zarządzane zasoby powinny zostać zlikwidowane; Fałsz w przeciwnym wypadku.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Kod generowany przez Projektanta formularzy systemu Windows
+
+        /// <summary>
+        /// Metoda wymagana do obsługi projektanta — nie należy modyfikować
+        /// jej zawartości w edytorze kodu.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.title = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.link = new System.Windows.Forms.LinkLabel();
+            this.choosingLabel = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.listBox1 = new System.Windows.Forms.ListBox();
+            this.SuspendLayout();
+            // 
+            // title
+            // 
+            this.title.AutoSize = true;
+            this.title.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.title.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.title.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.title.Font = new System.Drawing.Font("Arial Rounded MT Bold", 18F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.title.Location = new System.Drawing.Point(53, 20);
+            this.title.Name = "title";
+            this.title.Padding = new System.Windows.Forms.Padding(5);
+            this.title.Size = new System.Drawing.Size(711, 46);
+            this.title.TabIndex = 0;
+            this.title.Text = "This app implements Floyd-Warshall algorithm.\r\n";
+            this.title.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(77, 97);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(663, 17);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Firstly you should upload your graph to the project directory in forms of adjacen" +
+    "cy matrix. ";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(34, 125);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(754, 17);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "Remember that the graph should be directed. If you want to read more about this a" +
+    "lgorithm, click here:";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // link
+            // 
+            this.link.ActiveLinkColor = System.Drawing.Color.MistyRose;
+            this.link.AutoSize = true;
+            this.link.LinkColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(0)))), ((int)(((byte)(64)))));
+            this.link.Location = new System.Drawing.Point(184, 161);
+            this.link.Name = "link";
+            this.link.Size = new System.Drawing.Size(403, 16);
+            this.link.TabIndex = 3;
+            this.link.TabStop = true;
+            this.link.Text = "https://en.wikipedia.org/wiki/Floyd%E2%80%93Warshall_algorithm";
+            this.link.LinkClicked += new System.Windows.Forms.LinkLabelLinkClickedEventHandler(this.linkLabel1_LinkClicked);
+            // 
+            // choosingLabel
+            // 
+            this.choosingLabel.AutoSize = true;
+            this.choosingLabel.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.choosingLabel.Location = new System.Drawing.Point(220, 207);
+            this.choosingLabel.Name = "choosingLabel";
+            this.choosingLabel.Size = new System.Drawing.Size(337, 17);
+            this.choosingLabel.TabIndex = 4;
+            this.choosingLabel.Text = "Choose where you want to do your algorithm.";
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button1.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(80, 261);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(179, 37);
+            this.button1.TabIndex = 5;
+            this.button1.Text = "ASSEMBLER";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.SystemColors.ButtonShadow;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
+            this.button2.Font = new System.Drawing.Font("Arial Rounded MT Bold", 12F, System.Drawing.FontStyle.Italic, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button2.Location = new System.Drawing.Point(510, 261);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(169, 37);
+            this.button2.TabIndex = 6;
+            this.button2.Text = "C#\r\n";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Arial Rounded MT Bold", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(340, 324);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 17);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "Output:";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // listBox1
+            // 
+            this.listBox1.BackColor = System.Drawing.Color.SlateGray;
+            this.listBox1.FormattingEnabled = true;
+            this.listBox1.ItemHeight = 16;
+            this.listBox1.Location = new System.Drawing.Point(53, 354);
+            this.listBox1.Name = "listBox1";
+            this.listBox1.Size = new System.Drawing.Size(659, 84);
+            this.listBox1.TabIndex = 8;
+            // 
+            // Background
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.listBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.choosingLabel);
+            this.Controls.Add(this.link);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.title);
+            this.ForeColor = System.Drawing.SystemColors.Desktop;
+            this.Name = "Background";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label title;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.LinkLabel link;
+        private System.Windows.Forms.Label choosingLabel;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ListBox listBox1;
+    }
+}
+
