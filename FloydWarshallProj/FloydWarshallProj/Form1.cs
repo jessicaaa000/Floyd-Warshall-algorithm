5d85f4af79e20eddc55b76eff4f6a48b692fb292
Jessica Pawowska <jessica.pawlowska0@gmail.com>
2024-11-27 12:57:15 +0100

    Adding assembler and first draft of C# - configuration done properly

---
+++ FloydWarshallProj/FloydWarshallProj/Form1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FloydWarshallProj
+{
+    public partial class Background : Form
+    {
+        public Background()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
+
---
+++ FloydWarshallProj/FloydWarshallProj/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FloydWarshallProj
+{
+    internal static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+
+        [DllImport(@"C:\Users\jessi\Desktop\Floyd-Warshall-algorithm\FloydWarshallProj\x64\Debug\FloydWarshallAsm.dll")]
+        static extern int MyProc1(int a, int b);
+
+        [STAThread]
+        static void Main()
+        {
+            int x = 5, y = 3;
+            int retVal = MyProc1(x, y);
+            Console.Write("Moja pierwsza wartość obliczona w asm to:");
+            Console.WriteLine(retVal);
+            Console.ReadLine();
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Background());
+        }
+    }
+}
